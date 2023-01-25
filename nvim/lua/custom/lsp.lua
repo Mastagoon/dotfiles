115e6533e86590ae4eef09ca65a750eb6e8b45cc
Mastagoon <inzilagiway@gmail.com>
2023-01-26 02:39:32 +0300

    dart lsp

--- nvim/lua/custom/lsp.lua
+++ nvim/lua/custom/lsp.lua
@@ -86,6 +86,8 @@ require'lspconfig'.tailwindcss.setup{
 	flags = lsp_flags,
 }
 
+require'lspconfig'.dartls.setup{}
+
 --
 --Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
