3b1d19470d70a334bcc9eb3d7a9fd0ed09341fc3
Mastagoon <inzilagiway@gmail.com>
2023-04-25 14:27:13 +0300

    zellij

--- nvim/lua/custom/lsp.lua
+++ nvim/lua/custom/lsp.lua
@@ -88,6 +88,8 @@ require'lspconfig'.tailwindcss.setup{
 
 require'lspconfig'.dartls.setup{}
 
+require'lspconfig'.svelte.setup{}
+
 --
 --Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
