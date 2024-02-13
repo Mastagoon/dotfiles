65603a73780e78e29fd8996e0ee72a0ae00213c0
Mastagoon <inzilagiway@gmail.com>
2024-02-13 15:12:02 +0300

    no autoreformat

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
 local null_ls = require("null-ls")
 
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+-- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
 
@@ -12,15 +12,15 @@ null_ls.setup({
 			end, { buffer = bufnr, desc = "[lsp] format" })
 
 			-- format on save
-			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-			vim.api.nvim_create_autocmd(event, {
-				buffer = bufnr,
-				group = group,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr, async = async })
-				end,
-				desc = "[lsp] format on save",
-			})
+			-- vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+			-- vim.api.nvim_create_autocmd(event, {
+				-- buffer = bufnr,
+				-- group = group,
+				-- callback = function()
+					-- vim.lsp.buf.format({ bufnr = bufnr, async = async })
+				-- end,
+				-- desc = "[lsp] format on save",
+			-- })
 		end
 
 		if client.supports_method("textDocument/rangeFormatting") then
