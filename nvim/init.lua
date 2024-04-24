d4c761a5b36b5d84ed0570822c5eddab8b0e0851
Mastagoon <inzilagiway@gmail.com>
2024-04-24 19:11:25 +0300

    treesitter conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -843,6 +843,9 @@ require("lazy").setup({
 	{
 		"wakatime/vim-wakatime",
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
