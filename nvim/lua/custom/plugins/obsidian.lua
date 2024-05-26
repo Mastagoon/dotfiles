ea64b5c55d49c62e98f3fe7c4ee40d09ede82a27
Mastagoon <inzilagiway@gmail.com>
2024-05-26 21:15:26 +0300

    obsidian keybinds

--- nvim/lua/custom/plugins/obsidian.lua
+++ nvim/lua/custom/plugins/obsidian.lua
@@ -1,29 +1,27 @@
+vim.keymap.set("n", "<leader>os", ":ObsidianSearch<CR>", { desc = "Search in Obsidian" })
+vim.keymap.set("n", "<leader>ot", ":ObsidianToday<CR>", { desc = "Obsidian Today" })
+vim.keymap.set("n", "<leader>oy", ":ObsidianYesterday<CR>", { desc = "Obsidian Yesterday" })
+vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>", { desc = "Obsidian New" })
+
 return {
 	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	ft = "markdown",
-	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-	--   "BufReadPre path/to/my-vault/**.md",
-	--   "BufNewFile path/to/my-vault/**.md",
-	-- },
-	dependencies = {
+	tag = "*", -- recommended, use latest release instead of latest commit
+	requires = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
 
 		-- see below for full list of optional dependencies 👇
 	},
-	opts = {
-		workspaces = {
-			{
-				name = "the vault",
-				path = "~/projects/obsidian",
+	config = function()
+		require("obsidian").setup({
+			workspaces = {
+				{
+					name = "the vault",
+					path = "~/projects/obsidian",
+				},
 			},
-		},
 
-		-- see below for full list of options 👇
-	},
+			-- see below for full list of options 👇
+		})
+	end,
 }
